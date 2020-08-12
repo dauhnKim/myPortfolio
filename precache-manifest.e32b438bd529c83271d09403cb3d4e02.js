d18a4120b0e49adcad6450be0fcec7c868cf8419
Dauhn <dauhn357@gmail.com>
2020-08-12 22:04:33 +0900

    Updates

---
+++ precache-manifest.e32b438bd529c83271d09403cb3d4e02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a673eaaeeb82a51a0af177092bb263",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "3a0a2e66f2a628fa52ee",
+    "url": "/myportfolio/static/css/main.afbbb126.chunk.css"
+  },
+  {
+    "revision": "e52de6c571ace6f563c2",
+    "url": "/myportfolio/static/js/2.7505ca56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.7505ca56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0a2e66f2a628fa52ee",
+    "url": "/myportfolio/static/js/main.f78ca42b.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "fb52e6a292235c15d56b7d6f6d35c07c",
+    "url": "/myportfolio/static/media/about.fb52e6a2.png"
+  },
+  {
+    "revision": "06f0e0c89d2df0341a6be36c9f6ad120",
+    "url": "/myportfolio/static/media/pf01.06f0e0c8.png"
+  },
+  {
+    "revision": "d3aca6aa42b78e48ebedef3fd2541cbc",
+    "url": "/myportfolio/static/media/pf02.d3aca6aa.png"
+  },
+  {
+    "revision": "2bbe05d34de25b54c14757cbf849fa59",
+    "url": "/myportfolio/static/media/pf03.2bbe05d3.png"
+  }
+]);
