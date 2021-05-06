ab22c1a18964b25cb8f00406ee2545e25bfc2432
DauhnKIM <dauhn357@gmail.com>
2021-05-06 17:43:03 +0900

    Updates

---
+++ precache-manifest.1be09661c4f3cca00292743898869179.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed85388634b1659a896731b16a31459",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "5345f0140a355df56b0b",
+    "url": "/portfolio_react/static/css/main.ee47ea41.chunk.css"
+  },
+  {
+    "revision": "85d736998ab1c1b16d8c",
+    "url": "/portfolio_react/static/js/2.aec8b20d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_react/static/js/2.aec8b20d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5345f0140a355df56b0b",
+    "url": "/portfolio_react/static/js/main.04b30ebd.chunk.js"
+  },
+  {
+    "revision": "6aecdd57f541cfb08b5a",
+    "url": "/portfolio_react/static/js/runtime-main.fa04381e.js"
+  },
+  {
+    "revision": "8b84e20d045535d863062ded70920459",
+    "url": "/portfolio_react/static/media/about.8b84e20d.png"
+  },
+  {
+    "revision": "06f0e0c89d2df0341a6be36c9f6ad120",
+    "url": "/portfolio_react/static/media/pf01.06f0e0c8.png"
+  },
+  {
+    "revision": "d3aca6aa42b78e48ebedef3fd2541cbc",
+    "url": "/portfolio_react/static/media/pf02.d3aca6aa.png"
+  },
+  {
+    "revision": "2bbe05d34de25b54c14757cbf849fa59",
+    "url": "/portfolio_react/static/media/pf03.2bbe05d3.png"
+  }
+]);
